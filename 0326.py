756dfe4805a965d28f996309923623de9d1538a1
juyoung07 <yunju5090@gmail.com>
2024-03-28 00:21:40 +0900

    포맷 정수 표현

--- 0326.py
+++ 0326.py
@@ -8,22 +8,35 @@
 # print(s[30:])
 
 # 문자열 함수
-h = "  Happy Programming  "
-print(len(h))
-print(h.count("p"))
-print(h.upper())
-print(h.lower())
-print(h.strip())
-print(h.replace("Happy", "Funny"))
-print(h.find("ap"))
-print(h.rfind("a"))
-print("zoo")
+# h = "  Happy Programming  "
+# print(len(h))
+# print(h.count("p"))
+# print(h.upper())
+# print(h.lower())
+# print(h.strip())
+# print(h.replace("Happy", "Funny"))
+# print(h.find("ap"))
+# print(h.rfind("a"))
+# print("zoo")
+#
+# h = "  Happy Programming  "
+# print("a" in h)
+# print("amp" in h)
+# x = "01::23::ab::&&"
+# y = x.split("::")
+# print(y)
+# print(", " .join(y))
 
-h = "  Happy Programming  "
-print("a" in h)
-print("amp" in h)
-x = "01::23::ab::&&"
-y = x.split("::")
-print(y)
-print(", " .join(y))
+# format : 문자열 형식을 미리 정하고, 인자를 주어 문자열을 완성함
+# s = "name: {}, mumber: {}, soccer: {}"
+# print(s.format("Ronaldo", 7, True))
+# print("name: {name}, number: {num},".format(name = "Jordan", num=23))
+
+# 포맷 정수 표현
+print("'{:d}'".format(515))
+print("'{:5d}'".format(515))
+print("'{:+5d}'".format(515))
+print("'{:=+d}'".format(515))
+print("'{:05d}'".format(515))
+print("'{:+0d}'".format(515))
 
