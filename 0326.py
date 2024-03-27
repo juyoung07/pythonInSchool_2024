faeb61a168a64a9050dce0837d68d1c397a86b01
juyoung07 <yunju5090@gmail.com>
2024-03-28 00:19:17 +0900

    문자열 함수

--- 0326.py
+++ 0326.py
@@ -1,8 +1,20 @@
 # 슬라이싱 (원하는 부분을 가져옴 : 문자열[인덱스 n:인덱스 m] n부터 m까지(m바로 전까지))
-s = "Life is too short, You need Python."
-print(s[3:7])
-print(s[-10:-7])
-print(s[3:-10])
-print(s[-10:30])
-print(s[3:2])
-print(s[30:])
+# s = "Life is too short, You need Python."
+# print(s[3:7])
+# print(s[-10:-7])
+# print(s[3:-10])
+# print(s[-10:30])
+# print(s[3:2])
+# print(s[30:])
+
+# 문자열 함수
+h = "  Happy Programming  "
+print(len(h))
+print(h.count("p"))
+print(h.upper())
+print(h.lower())
+print(h.strip())
+print(h.replace("Happy", "Funny"))
+print(h.find("ap"))
+print(h.rfind("a"))
+print("zoo")
