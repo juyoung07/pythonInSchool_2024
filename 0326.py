f269eb757574556767c2bd37868f4a6bd25fcee1
juyoung07 <yunju5090@gmail.com>
2024-03-28 00:18:09 +0900

    슬라이싱

---
+++ 0326.py
@@ -0,0 +1,8 @@
+# 슬라이싱 (원하는 부분을 가져옴 : 문자열[인덱스 n:인덱스 m] n부터 m까지(m바로 전까지))
+s = "Life is too short, You need Python."
+print(s[3:7])
+print(s[-10:-7])
+print(s[3:-10])
+print(s[-10:30])
+print(s[3:2])
+print(s[30:])
