e2d25a628cf0c43dbe9db43584c2023e424a921e
juyoung07 <yunju5090@gmail.com>
2024-03-28 00:24:11 +0900

    리스트에 요소 추가

--- 0326.py
+++ 0326.py
@@ -50,11 +50,25 @@
 
 # 리스트 : [], 튜플 : (), 딕셔너리 : {키:값, 키:값}, {값, 값}
 # 리스트 생성
-l : []
-player = ["Messi", 10, True]
-print(list("happy"))
-print(list((1125, 2436)))
-print(list({"menu":"pizza","price":10000}))
-print(list({"사과", "배"}))
+# l : []
+# player = ["Messi", 10, True]
+# print(list("happy"))
+# print(list((1125, 2436)))
+# print(list({"menu":"pizza","price":10000}))
+# print(list({"사과", "배"}))
+# nums = list(range(3))
+
+# 리스트에 요소 추가
 nums = list(range(3))
+print(nums+[10, 11])
+nums += [10, 11]
+print(nums)
+nums.append(20)
+print(nums)
+nums.append([30, 31])
+print(nums)
+nums.insert(2, 40)
+print(nums)
+nums.extend([50, 51])
+print(nums)
 
